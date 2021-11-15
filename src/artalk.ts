9c7bcc6e959f78eea9f5919c41af88968385f69c
qwqcode <qwqcode@gmail.com>
2021-11-15 14:52:19 +0800

    fix: default pageKey generate.
    Signed-off-by: qwqcode <qwqcode@gmail.com>

--- src/artalk.ts
+++ src/artalk.ts
@@ -33,7 +33,7 @@ export default class Artalk {
 
     // 默认 pageKey
     if (!this.conf.pageKey) {
-      this.conf.pageKey = `${window.location.protocol}//'${window.location.host}/${window.location.pathname}`
+      this.conf.pageKey = `${window.location.protocol}//${window.location.host}${window.location.pathname}`
     }
 
     // 装载元素

211585016da4aeeb4e0d8b2c13e1347021d74ace
qwqcode <qwqcode@gmail.com>
2022-02-26 20:55:47 +0800

    feat: admin_users password support to be encrypted by using bcrypt.
    Signed-off-by: qwqcode <qwqcode@gmail.com>

--- http/api_user_login.go
+++ http/api_user_login.go
@@ -1,8 +1,11 @@
 package http
 
 import (
+	"strings"
+
 	"github.com/ArtalkJS/ArtalkGo/model"
 	"github.com/labstack/echo/v4"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type ParamsLogin struct {
@@ -21,11 +24,32 @@ func ActionLogin(c echo.Context) error {
 	RecordAction(c)
 
 	user := model.FindUser(p.Name, p.Email) // name = ? OR email = ?
-	if user.IsEmpty() || user.Password != p.Password {
+
+	if user.IsEmpty() {
+		return RespError(c, "验证失败")
+	}
+
+	// 密码验证
+	bcryptPrefix := "(bcrypt)"
+	if strings.HasPrefix(user.Password, bcryptPrefix) {
+		err := bcrypt.CompareHashAndPassword(
+			[]byte(strings.TrimPrefix(user.Password, bcryptPrefix)),
+			[]byte(p.Password),
+		)
+
+		if err != nil {
+			return RespError(c, "验证失败")
+		}
+	} else if user.Password != p.Password {
 		return RespError(c, "验证失败")
 	}
 
 	return RespData(c, Map{
 		"token": LoginGetUserToken(user),
 	})
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
 }

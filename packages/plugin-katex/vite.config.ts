59ebfd3d6c8bb345290b3ee3fbaca71866fc3dfa
qwqcode <qwqcode@gmail.com>
2022-04-21 19:17:29 +0800

    chore(plugin-katex): simplify the code
    Signed-off-by: qwqcode <qwqcode@gmail.com>

--- packages/plugin-katex/vite.config.ts
+++ packages/plugin-katex/vite.config.ts
@@ -18,9 +18,6 @@ export default defineConfig({
       // 确保外部化处理那些你不想打包进库的依赖
       external: ['artalk', 'katex'],
       output: {
-        assetFileNames: assetInfo => {
-          return "[name].[ext]"
-        },
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
           artalk: 'Artalk',

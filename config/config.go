dbc8e268bc1b98367980fc583a245f20aadaf2be
qwqcode <qwqcode@gmail.com>
2022-05-23 00:50:59 +0800

    chore(conf): Prevent deprecated options from unmarshall in JSON
    Signed-off-by: qwqcode <qwqcode@gmail.com>

--- config/config.go
+++ config/config.go
@@ -13,7 +13,7 @@ type Config struct {
 	DB             DBConf           `mapstructure:"db" json:"db"`                           // 数据文件
 	Cache          CacheConf        `mapstructure:"cache" json:"cache"`                     // 缓存
 	Log            LogConf          `mapstructure:"log" json:"log"`                         // 日志文件
-	AllowOrigins   []string         `mapstructure:"allow_origins" json:"allow_origins"`     // @deprecated 已废弃 (请使用 TrustedDomains)
+	AllowOrigins   []string         `mapstructure:"allow_origins" json:"-"`                 // @deprecated 已废弃 (请使用 TrustedDomains)
 	TrustedDomains []string         `mapstructure:"trusted_domains" json:"trusted_domains"` // 可信任的域名 (新)
 	SSL            SSLConf          `mapstructure:"ssl" json:"ssl"`                         // SSL
 	SiteDefault    string           `mapstructure:"site_default" json:"site_default"`       // 默认站点名（当请求无指定 site_name 时使用）
@@ -24,7 +24,7 @@ type Config struct {
 	Email          EmailConf        `mapstructure:"email" json:"email"`                     // 邮箱提醒
 	ImgUpload      ImgUploadConf    `mapstructure:"img_upload" json:"img_upload"`           // 图片上传
 	AdminNotify    AdminNotifyConf  `mapstructure:"admin_notify" json:"admin_notify"`       // 其他通知方式
-	Notify         *AdminNotifyConf `mapstructure:"notify" json:"notify"`                   // @deprecated 已废弃 (请使用 AdminNotify)
+	Notify         *AdminNotifyConf `mapstructure:"notify" json:"-"`                        // @deprecated 已废弃 (请使用 AdminNotify)
 	Frontend       FrontendConf     `mapstructure:"frontend" json:"frontend"`
 }
 
@@ -124,7 +124,7 @@ type KeyWordsAntispamConf struct {
 type CaptchaConf struct {
 	Enabled       bool        `mapstructure:"enabled" json:"enabled"`
 	Always        bool        `mapstructure:"always" json:"always"`
-	ActionTimeout int         `mapstructure:"action_timeout" json:"action_timeout"` // @deprecated 已废弃 (请使用 ActionReset)
+	ActionTimeout int         `mapstructure:"action_timeout" json:"-"` // @deprecated 已废弃 (请使用 ActionReset)
 	ActionReset   int         `mapstructure:"action_reset" json:"action_reset"`
 	ActionLimit   int         `mapstructure:"action_limit" json:"action_limit"`
 	Geetest       GeetestConf `mapstructure:"geetest" json:"geetest"`
@@ -137,15 +137,15 @@ type GeetestConf struct {
 }
 
 type EmailConf struct {
-	Enabled            bool            `mapstructure:"enabled" json:"enabled"`                             // 总开关
-	SendType           EmailSenderType `mapstructure:"send_type" json:"send_type"`                         // 发送方式
-	SendName           string          `mapstructure:"send_name" json:"send_name"`                         // 发件人名
-	SendAddr           string          `mapstructure:"send_addr" json:"send_addr"`                         // 发件人地址
-	MailSubject        string          `mapstructure:"mail_subject" json:"mail_subject"`                   // 邮件标题
-	MailSubjectToAdmin string          `mapstructure:"mail_subject_to_admin" json:"mail_subject_to_admin"` // @deprecated 已废弃 (请使用 AdminNotify.Email.MailSubject) - 邮件标题 (发送给管理员用)
-	MailTpl            string          `mapstructure:"mail_tpl" json:"mail_tpl"`                           // 邮件模板
-	SMTP               SMTPConf        `mapstructure:"smtp" json:"smtp"`                                   // SMTP 配置
-	AliDM              AliDMConf       `mapstructure:"ali_dm" json:"ali_dm"`                               // 阿里云邮件配置
+	Enabled            bool            `mapstructure:"enabled" json:"enabled"`           // 总开关
+	SendType           EmailSenderType `mapstructure:"send_type" json:"send_type"`       // 发送方式
+	SendName           string          `mapstructure:"send_name" json:"send_name"`       // 发件人名
+	SendAddr           string          `mapstructure:"send_addr" json:"send_addr"`       // 发件人地址
+	MailSubject        string          `mapstructure:"mail_subject" json:"mail_subject"` // 邮件标题
+	MailSubjectToAdmin string          `mapstructure:"mail_subject_to_admin" json:"-"`   // @deprecated 已废弃 (请使用 AdminNotify.Email.MailSubject) - 邮件标题 (发送给管理员用)
+	MailTpl            string          `mapstructure:"mail_tpl" json:"mail_tpl"`         // 邮件模板
+	SMTP               SMTPConf        `mapstructure:"smtp" json:"smtp"`                 // SMTP 配置
+	AliDM              AliDMConf       `mapstructure:"ali_dm" json:"ali_dm"`             // 阿里云邮件配置
 }
 
 type SMTPConf struct {

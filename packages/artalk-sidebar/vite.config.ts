05ee606e4fd53832663d54f4f403eb8e4a038727
qwqcode <qwqcode@gmail.com>
2022-09-26 22:59:47 +0800

    fix(sidebar): add base public path for build
    Signed-off-by: qwqcode <qwqcode@gmail.com>

--- packages/artalk-sidebar/vite.config.ts
+++ packages/artalk-sidebar/vite.config.ts
@@ -9,6 +9,7 @@ import { HeadlessUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   build: {
     target: 'es2015',
     outDir: resolve(__dirname, "dist"),

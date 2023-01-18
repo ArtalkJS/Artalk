0f8d3f935a4cc2da0a954d23a0a24d0539bd8460
pluveto <i@pluvet.com>
2023-01-18 12:30:00 +0000

    refactor(scripts): double quote to prevent globbing and word splitting

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -9,10 +9,10 @@ if [[ $* == *--push* ]]
 then
     # tag and push image
     for tag in {${VERSION},latest}; do
-        docker image tag ${IMAGE_NAME} ${IMAGE_NAME}:${tag}
-        docker push ${IMAGE_NAME}:${tag}
+        docker image tag "${IMAGE_NAME}" "${IMAGE_NAME}:${tag}"
+        docker push "${IMAGE_NAME}:${tag}"
     done
 else
     # build
-    docker image build -t ${IMAGE_NAME} .
+    docker image build -t "${IMAGE_NAME}" .
 fi

273df47f7d6be9381a50dcc9ccea0615dd783787
qwqcode <qwqcode@gmail.com>
2023-03-15 20:58:20 +0800

    fix(ui/sort-dropdown): dropdown menu disappears after call reload func
    Signed-off-by: qwqcode <qwqcode@gmail.com>

--- ui/packages/artalk/src/list/list-ui.ts
+++ ui/packages/artalk/src/list/list-ui.ts
@@ -43,7 +43,6 @@ export function renderErrorDialog(list: ListLite, errMsg: string, errData?: any)
       create_name: list.ctx.conf.site,
       create_urls: `${window.location.protocol}//${window.location.host}`
     }
-    // TODO 真的是飞鸽传书啊
     sidebarView = `sites|${JSON.stringify(viewLoadParam)}`
   }
 
@@ -77,4 +76,61 @@ export function versionCheckDialog(list: ListLite, feVer: string, beVer: string)
   }
 
   return false
+}
+
+/** 评论排序方式选择下拉菜单 */
+export function renderDropdown(conf: {
+  $dropdownWrap: HTMLElement,
+  dropdownList: [string, () => void][]
+}) {
+  const { $dropdownWrap, dropdownList } = conf
+  if ($dropdownWrap.querySelector('.atk-dropdown')) return
+
+  // 修改 class
+  $dropdownWrap.classList.add('atk-dropdown-wrap')
+
+  // 插入图标
+  $dropdownWrap.append(Utils.createElement(`<span class="atk-arrow-down-icon"></span>`))
+
+  // 列表项点击事件
+  let curtActive = 0 // 当前选中
+  const onItemClick = (i: number, $item: HTMLElement, name: string, action: Function) => {
+    action()
+
+    // set active
+    curtActive = i
+    $dropdown.querySelectorAll('.active').forEach((e) => { e.classList.remove('active') })
+    $item.classList.add('active')
+
+    // 关闭层 (临时消失，取消 :hover)
+    $dropdown.style.display = 'none'
+    setTimeout(() => { $dropdown.style.display = '' }, 80)
+  }
+
+  // 生成列表元素
+  const $dropdown = Utils.createElement(`<ul class="atk-dropdown atk-fade-in"></ul>`)
+  dropdownList.forEach((item, i) => {
+    const name = item[0] as string
+    const action = item[1] as Function
+
+    const $item = Utils.createElement(`<li class="atk-dropdown-item"><span></span></li>`)
+    const $link = $item.querySelector<HTMLElement>('span')!
+    $link.innerText = name
+    $link.onclick = () => { onItemClick(i, $item, name, action) }
+    $dropdown.append($item)
+
+    if (i === curtActive) $item.classList.add('active') // 默认选中项
+  })
+
+  $dropdownWrap.append($dropdown)
+}
+
+/** 删除评论排序方式选择下拉菜单 */
+export function removeDropdown(conf: {
+  $dropdownWrap: HTMLElement
+}) {
+  const { $dropdownWrap } = conf
+  $dropdownWrap.classList.remove('atk-dropdown-wrap')
+  $dropdownWrap.querySelector('.atk-arrow-down-icon')?.remove()
+  $dropdownWrap.querySelector('.atk-dropdown')?.remove()
 }

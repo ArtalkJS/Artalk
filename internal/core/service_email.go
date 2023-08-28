f5e2dc3467b3b73fd6440765014c194515bb9362
qwqcode <qwqcode@gmail.com>
2023-08-29 01:11:01 +0800

    refactor(core): refactor core pkg

---
+++ internal/core/service_email.go
@@ -0,0 +1,108 @@
+package core
+
+import (
+	"time"
+
+	"github.com/ArtalkJS/Artalk/internal/email"
+	"github.com/ArtalkJS/Artalk/internal/entity"
+	"github.com/ArtalkJS/Artalk/internal/log"
+)
+
+var _ Service = (*EmailService)(nil)
+
+type EmailService struct {
+	app   *App
+	queue *email.EmailQueue
+}
+
+func NewEmailService(app *App) *EmailService {
+	return &EmailService{app: app}
+}
+
+func (e *EmailService) Init() error {
+	e.queue = email.NewQueue(&email.EmailConf{
+		EmailConf: e.app.Conf().Email,
+		OnSendSuccess: func(email *email.Email) {
+			if email.LinkedNotify == nil {
+				log.Debug("email.LinkedNotify is null")
+				return
+			}
+
+			if err := e.app.Dao().NotifySetEmailed(email.LinkedNotify); err != nil { // flag associated comment as emailed
+				log.Errorf("[Email] Flag email delivery status for associated comment failed: %s", err)
+			}
+		},
+	})
+
+	return nil
+}
+
+func (e *EmailService) Dispose() error {
+	e.queue.Close()
+
+	return nil
+}
+
+func (e *EmailService) AsyncSendTo(subject string, body string, toAddr string) {
+	if !e.app.Conf().Email.Enabled {
+		return
+	}
+
+	e.queue.Push(&email.Email{
+		FromAddr: e.app.Conf().Email.SendAddr,
+		FromName: e.app.Conf().Email.SendName,
+		ToAddr:   toAddr,
+		Subject:  subject,
+		Body:     body,
+	})
+}
+
+func (e *EmailService) GetRender(useAdminTplParam ...bool) *email.Render {
+	useAdminTpl := false
+	if len(useAdminTplParam) > 0 {
+		useAdminTpl = useAdminTplParam[0]
+	}
+
+	mailTplName := e.app.Conf().Email.MailTpl
+
+	// 发送给管理员的邮件单独使用管理员邮件模板
+	adminTpl := e.app.Conf().AdminNotify.Email.MailTpl
+	if useAdminTpl && adminTpl != "" {
+		mailTplName = adminTpl
+	}
+
+	// create new email render instance
+	render := email.NewRender(e.app.Dao(), mailTplName)
+
+	return render
+}
+
+func (e *EmailService) AsyncSend(notify *entity.Notify) {
+	if !e.app.Conf().Email.Enabled {
+		return
+	}
+
+	receiveUser := e.app.Dao().FetchUserForNotify(notify)
+	render := e.GetRender(receiveUser.IsAdmin)
+
+	// render email body
+	mailBody := render.RenderEmailBody(notify)
+	mailSubject := ""
+	if !receiveUser.IsAdmin {
+		mailSubject = render.RenderCommon(e.app.Conf().Email.MailSubject, notify)
+	} else {
+		mailSubject = render.RenderCommon(e.app.Conf().AdminNotify.Email.MailSubject, notify)
+	}
+
+	log.Debug(time.Now(), " "+receiveUser.Email)
+
+	// add email send task to queue
+	e.queue.Push(&email.Email{
+		FromAddr:     e.app.Conf().Email.SendAddr,
+		FromName:     render.RenderCommon(e.app.Conf().Email.SendName, notify),
+		ToAddr:       receiveUser.Email,
+		Subject:      mailSubject,
+		Body:         mailBody,
+		LinkedNotify: notify,
+	})
+}
---
+++ internal/core/service_ip_region.go
@@ -0,0 +1,30 @@
+package core
+
+import "github.com/ArtalkJS/Artalk/internal/ip_region"
+
+var _ Service = (*IPRegionService)(nil)
+
+type IPRegionService struct {
+	app *App
+}
+
+func NewIPRegionService(app *App) *IPRegionService {
+	return &IPRegionService{app: app}
+}
+
+func (s *IPRegionService) Init() error {
+	return nil
+}
+
+func (s *IPRegionService) Dispose() error {
+	return nil
+}
+
+func (s *IPRegionService) Query(ip string) string {
+	client := ip_region.NewIPRegion(ip_region.IPRegionConf{
+		IPRegionConf: s.app.Conf().IPRegion,
+		CacheEnabled: s.app.Conf().Cache.Enabled,
+	})
+
+	return client.IP2Region(ip)
+}

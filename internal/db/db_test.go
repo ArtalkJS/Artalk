e708ad1561b39131c1f697730ed45214adac7bc7
qwqcode <qwqcode@gmail.com>
2023-08-31 14:37:19 +0800

    test(db): add tests for db

---
+++ internal/db/db_test.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"testing"
+
+	"github.com/ArtalkJS/Artalk/internal/config"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewDB(t *testing.T) {
+	db, err := NewDB(config.DBConf{
+		Type:        config.TypeSQLite,
+		Dsn:         "file::memory:?cache=shared",
+		TablePrefix: "atk_",
+	})
+	defer CloseDB(db)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.NotNil(t, db)
+}
+
+func TestNewTestDB(t *testing.T) {
+	db, err := NewTestDB()
+	defer CloseDB(db)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.NotNil(t, db)
+}

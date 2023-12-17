7286642fc98aa9c8d1cf578f8b7be51d5c1c3d82
qwqcode <22412567+qwqcode@users.noreply.github.com>
2023-12-18 00:46:08 +0800

    fix(ui): static method export issue in JS module (#669)

--- ui/packages/artalk/src/main.ts
+++ ui/packages/artalk/src/main.ts
@@ -3,3 +3,12 @@ import * as ArtalkType from '../types'
 
 export { ArtalkType }
 export default Artalk
+
+// Expose the static methods from the Artalk class
+// because direct export of static methods is not supported
+// for adapting to different environments like CommonJS and browser IIFE
+// for example, we can use `Artalk.init()` rather than `Artalk.default.init()`
+// therefore, we need to manually expose the static methods in the Artalk class.
+export const init = Artalk.init
+export const use = Artalk.use
+export const loadCountWidget = Artalk.loadCountWidget

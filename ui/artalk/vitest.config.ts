b74e5cf94cc8b9f9f0d72cff821db000e6532295
qwqcode <qwqcode@gmail.com>
2023-12-26 00:03:08 +0800

    refactor(ui/test): add vitest for unit testing

---
+++ ui/artalk/vitest.config.ts
@@ -0,0 +1,16 @@
+import { fileURLToPath } from 'node:url'
+import { resolve } from 'path'
+import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import viteConfig from './vite.config'
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      exclude: [...configDefaults.exclude, 'tests/e2e/*'],
+      root: fileURLToPath(new URL('./', import.meta.url)),
+      setupFiles: [resolve(__dirname, '/tests/setup.ts')],
+    }
+  })
+)

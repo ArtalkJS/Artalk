5d20ff1ef52b43795b9f3f0df8993c3bd9888032
qwqcode <qwqcode@gmail.com>
2024-02-06 15:55:27 +0800

    chore(fiber_logger): correct the http status code range for err log

--- server/server.go
+++ server/server.go
@@ -57,11 +57,11 @@ func Serve(app *core.App) (*fiber.App, error) {
 		Format: "[${status}] ${method} ${path} ${latency} ${ip} ${reqHeader:X-Request-ID} ${referer} ${ua}\n",
 		Output: io.Discard,
 		Done: func(c *fiber.Ctx, logString []byte) {
-			statusOK := c.Response().StatusCode() >= 200 && c.Response().StatusCode() <= 302
-			if !statusOK {
-				log.StandardLogger().WriterLevel(log.ErrorLevel).Write(logString)
-			} else {
+			code := c.Response().StatusCode()
+			if (code >= 200 && code <= 299) || (code >= 300 && code <= 308) {
 				log.StandardLogger().WriterLevel(log.DebugLevel).Write(logString)
+			} else {
+				log.StandardLogger().WriterLevel(log.ErrorLevel).Write(logString)
 			}
 		},
 	}))

c482e19f1983ce4d96072aa42ecb3c6923dcd324
qwqcode <qwqcode@gmail.com>
2024-04-07 20:10:19 +0800

    chore(sidebar): fix build type issue when upgrade deps
    https://github.com/posva/unplugin-vue-router/issues/353

--- ui/artalk-sidebar/auto-imports.d.ts
+++ ui/artalk-sidebar/auto-imports.d.ts
@@ -12,7 +12,7 @@ declare global {
   const defineAsyncComponent: typeof import('vue')['defineAsyncComponent']
   const defineComponent: typeof import('vue')['defineComponent']
   const defineLoader: typeof import('vue-router/auto')['defineLoader']
-  const definePage: typeof import('unplugin-vue-router/runtime')['_definePage']
+  const definePage: typeof import('unplugin-vue-router/runtime')['definePage']
   const effectScope: typeof import('vue')['effectScope']
   const getCurrentInstance: typeof import('vue')['getCurrentInstance']
   const getCurrentScope: typeof import('vue')['getCurrentScope']

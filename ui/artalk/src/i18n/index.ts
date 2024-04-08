957e84d933882de5f22e664015a0617604e7efed
Mister-Hope <mister-hope@outlook.com>
2024-04-08 16:16:11 +0800

    chore: fix

--- ui/artalk/src/i18n/index.ts
+++ ui/artalk/src/i18n/index.ts
@@ -9,7 +9,7 @@ export type * from './en'
 // https://en.wikipedia.org/wiki/ISO_639
 // https://datatracker.ietf.org/doc/html/rfc5646#section-2.1
 export const internal = {
-  en: en,
+  en,
   'en-US': en,
   'zh-CN': zhCN,
 }

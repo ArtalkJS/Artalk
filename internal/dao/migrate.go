f18070e21fae6417f39836e92b7c79d125afb678
qwqcode <qwqcode@gmail.com>
2024-04-30 14:10:38 +0800

    feat(db/migrator): provide tool for merging duplicate page

--- internal/dao/migrate.go
+++ internal/dao/migrate.go
@@ -1,6 +1,8 @@
 package dao
 
 import (
+	"os"
+
 	"github.com/ArtalkJS/Artalk/internal/entity"
 	"github.com/ArtalkJS/Artalk/internal/log"
 )
@@ -21,6 +23,11 @@ func (dao *Dao) MigrateModels() {
 	// because there are many different DBs and the implementation of foreign keys may be different,
 	// and the DB may not support foreign keys, so don't rely on the foreign key function of the DB system.
 	dao.DropConstraintsIfExist()
+
+	// Merge pages
+	if os.Getenv("ATK_DB_MIGRATOR_FUNC_MERGE_PAGES") == "1" {
+		dao.MergePages()
+	}
 }
 
 // Remove all constraints
@@ -86,4 +93,48 @@ func (dao *Dao) MigrateRootID() {
 	dao.DB().Table("comments").Where("id = root_id").Update("root_id", 0)
 
 	log.Info(TAG, "Root IDs generated successfully.")
+}
+
+func (dao *Dao) MergePages() {
+	// merge pages with same key and site_name, sum pv
+	pages := []*entity.Page{}
+
+	// load all pages
+	if err := dao.DB().Order("id ASC").Find(&pages).Error; err != nil {
+		log.Fatal("Failed to load pages. ", err.Error)
+	}
+	beforeLen := len(pages)
+
+	// merge pages
+	mergedPages := map[string]*entity.Page{}
+	for _, page := range pages {
+		key := page.SiteName + page.Key
+		if _, ok := mergedPages[key]; !ok {
+			mergedPages[key] = page
+		} else {
+			mergedPages[key].PV += page.PV
+			mergedPages[key].VoteUp += page.VoteUp
+			mergedPages[key].VoteDown += page.VoteDown
+		}
+	}
+
+	// delete all pages
+	dao.DB().Exec("DELETE FROM pages")
+
+	// insert merged pages
+	pages = []*entity.Page{}
+	for _, page := range mergedPages {
+		pages = append(pages, page)
+	}
+	if err := dao.DB().CreateInBatches(pages, 1000); err.Error != nil {
+		log.Fatal("Failed to insert merged pages. ", err.Error)
+	}
+
+	// drop page AccessibleURL column
+	if dao.DB().Migrator().HasColumn(&entity.Page{}, "accessible_url") {
+		dao.DB().Migrator().DropColumn(&entity.Page{}, "accessible_url")
+	}
+
+	log.Info("Pages merged successfully. Before pages: ", beforeLen, ", After pages: ", len(mergedPages), ", Deleted pages: ", beforeLen-len(mergedPages))
+	os.Exit(0)
 }

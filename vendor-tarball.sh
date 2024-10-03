bbfd6c3cd74d80ca6bfa59d2fbdfd42101bb25c7
duli <duli4868@gmail.com>
2024-10-03 21:40:41 +0800

    perf: improve vendor-tarball.sh to make it reproducible

--- vendor-tarball.sh
+++ vendor-tarball.sh
@@ -10,12 +10,33 @@ fi
 set -euo pipefail
 
 version=${tag#v}
-
 echo "Using version: $version"
 
-git -c advice.detachedHead=false clone --branch $tag --depth 1 https://github.com/artalkjs/artalk/ $name-$version
-pushd $name-$version
+rm -rf "$name-$version"
+
+# Clone the repository
+git -c advice.detachedHead=false clone --branch "$tag" --depth 1 https://github.com/artalkjs/artalk/ "$name-$version"
+pushd "$name-$version"
+
+# Vendor dependencies
 GOPROXY='https://proxy.golang.org,direct' go mod vendor
 popd
-tar --exclude .git -czf $name-$version-vendored.tar.gz $name-$version
-rm -rf $name-$version
+
+# More reproducible!
+TARFLAGS=(
+  --exclude .git
+  --sort=name
+  --format=posix
+  --pax-option=delete=atime,delete=ctime
+  --clamp-mtime
+  --mtime='1970-01-01 00:00:00 UTC'
+  --numeric-owner
+  --owner=0
+  --group=0
+  --mode=go+u,go-w
+)
+
+tar "${TARFLAGS[@]}" -czvf "$name-$version-vendored.tar.gz" "$name-$version"
+
+# Clean up the temporary directory
+rm -rf "$name-$version"

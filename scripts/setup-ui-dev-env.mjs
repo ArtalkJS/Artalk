da4c0935d5ed11c0ce22add5a1c773469107b1dd
qwqcode <qwqcode@gmail.com>
2024-10-05 17:39:42 +0800

    update setup-ui-dev-env.mjs

--- scripts/setup-ui-dev-env.mjs
+++ scripts/setup-ui-dev-env.mjs
@@ -1,4 +1,9 @@
 import { exec } from 'child_process'
+import { accessSync } from 'fs'
+import { resolve } from 'path'
+
+const __dirname = new URL('.', import.meta.url).pathname
+const forceBuild = process.env.ATK_DEV_TOOLCHAIN_FORCE_BUILD === '1'
 
 function runCommand(command) {
   return new Promise((resolve, reject) => {
@@ -12,15 +17,30 @@ function runCommand(command) {
   })
 }
 
+function checkFolderExists(pathname) {
+  if (forceBuild) return false
+  try {
+    accessSync(resolve(__dirname, '../', pathname))
+    return true
+  } catch (error) {
+    return false
+  }
+}
+
+const green = '\x1b[32m'
+
 async function build() {
   try {
     // Build Artalk Plugin Kit for plugin development
-    await runCommand('pnpm build:plugin-kit')
-    const green = '\x1b[32m'
-    console.log(green, '[ArtalkDev] build @artalk/plugin-kit success')
+    if (!checkFolderExists('ui/plugin-kit/dist')) {
+      await runCommand('pnpm build:plugin-kit')
+      console.log(green, '[ArtalkDev] build @artalk/plugin-kit success')
+    }
     // Build Artalk eslint plugin for lint checking
-    await runCommand('pnpm build:eslint-plugin')
-    console.log(green, '[ArtalkDev] build eslint-plugin-artalk success')
+    if (!checkFolderExists('ui/eslint-plugin-artalk/dist')) {
+      await runCommand('pnpm build:eslint-plugin')
+      console.log(green, '[ArtalkDev] build eslint-plugin-artalk success')
+    }
   } catch (error) {
     console.error('[ArtalkDev] Artalk UI development environment setup failed:', error)
   }
